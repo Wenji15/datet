83c77a6fa8c46b6828cd096c3985ff41c5708dd3
Wenji15 <122006741+Wenji15@users.noreply.github.com>
2024-11-18 15:51:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,11 @@
 import streamlit as st
-from chardet import detect
+# from chardet import detect
 import json
 
 def read_json(file_path):
     # try:
     with open(file_path, 'rb') as file:
         file_content = file.read()
-        encoding = detect(file_content)['encoding']
-
-        # 如果检测失败，默认使用 utf-8
-        if not encoding:
-            encoding = 'utf-8'
-
-        content = file_content.decode(encoding)
         data = json.loads(content)
         return data
 
