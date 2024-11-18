a3f142fb23bfb9f732c401a7a1e17032d7fff914
Wenji15 <122006741+Wenji15@users.noreply.github.com>
2024-11-18 15:57:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import pandas as pd
 
 st.set_page_config(page_title="数据后处理过程演示", layout="wide")
 config_file_path = st.text_input('输入配置文件路径', help='json', )
 if st.button("读取配置文件"):
-    json_file = pd.read_csv(config_file_path)
-    st.write(json_file.head())
+    st.write(config_file_path)
