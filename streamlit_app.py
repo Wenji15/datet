156455e029b1ba517d9f87c9b0901380e88e176d
Wenji15 <122006741+Wenji15@users.noreply.github.com>
2024-11-18 15:53:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,8 @@
 import streamlit as st
-import json
-
-def read_json(file_path):
-    # try:
-    with open(file_path, 'rb') as file:
-        file_content = file.read()
-        data = json.loads(file_content)
-        return data
+import pandas as pd
 
 st.set_page_config(page_title="数据后处理过程演示", layout="wide")
 config_file_path = st.text_input('输入配置文件路径', help='json', )
 if st.button("读取配置文件"):
-    json_file = read_json(config_file_path)
-    st.write(json_file)
+    json_file = pd.read_csv(config_file_path)
+    st.write(json_file.head())
